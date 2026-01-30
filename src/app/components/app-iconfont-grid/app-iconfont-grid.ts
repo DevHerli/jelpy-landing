55da136b90097d0ed488bf7bc9b3ee1dbd674d6d
PanqueDev <ilovepanquenwn@gmail.com>
2026-01-29 23:35:27 -0700

    feat: add copyToClipboard method to AppIconfontGrid for copying icon HTML

--- src/app/components/app-iconfont-grid/app-iconfont-grid.ts
+++ src/app/components/app-iconfont-grid/app-iconfont-grid.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class AppIconfontGrid {
-
+  public copyToClipboard(texto: string): void {
+    const textoCopiado: string = "<span class='" + texto + "'></span>";
+    navigator.clipboard.writeText(textoCopiado)
+      .then(() => {
+        console.log("Texto copiado correctamente ✅");
+      })
+      .catch(err => {
+        console.error("Error al copiar ❌", err);
+      });
+  }
 }
